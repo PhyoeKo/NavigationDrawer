35af2c9dc9f55a4f2091007c58f3755d356a2760
Phyoe Ko <developer.skylark@skylarkmyanmar.com>
2019-05-20 15:50:20 +0630

    added recycler view

---
+++ app/src/main/java/com/example/navigation/MovieAdapter.java
@@ -0,0 +1,60 @@
+package com.example.navigation;
+
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.List;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MyViewHolder> {
+    private List<Model> moviesList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView title, year, genre;
+
+        public MyViewHolder(View view) {
+            super(view);
+            title =  view.findViewById(R.id.title);
+            genre =  view.findViewById(R.id.genre);
+            year =  view.findViewById(R.id.year);
+        }
+    }
+
+
+    public MovieAdapter(List<Model> moviesList) {
+        this.moviesList = moviesList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+              View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_row, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        final Model movie = moviesList.get(position);
+        holder.title.setText(movie.getTitle());
+        holder.genre.setText(movie.getGenre());
+        holder.year.setText(movie.getYear());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+             Toast.makeText(v.getContext(),"You Clicked "+ movie.getTitle(),Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return moviesList.size();
+    }
+}
--- app/src/main/java/com/example/navigation/ui/fragment/GalleryFragment.java
+++ app/src/main/java/com/example/navigation/ui/fragment/GalleryFragment.java
@@ -2,12 +2,23 @@ package com.example.navigation.ui.fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.navigation.Model;
+import com.example.navigation.MovieAdapter;
 import com.example.navigation.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -15,6 +26,9 @@ import com.example.navigation.R;
  */
 public class GalleryFragment extends Fragment {
 
+    private List<Model> movieList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private MovieAdapter mAdapter;
 
     public GalleryFragment() {
         // Required empty public constructor
@@ -26,6 +40,74 @@ public class GalleryFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_gallery, container, false);
+
+
+
+
     }
 
+    @Override
+    public void onViewCreated( View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mAdapter = new MovieAdapter(movieList);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recycler);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+
+        prepareMovieData();
+
+    }
+    private void prepareMovieData() {
+        Model movie = new Model("Mad Max: Fury Road", "Action & Adventure", "2015");
+        movieList.add(movie);
+
+        movie = new Model("Inside Out", "Animation, Kids & Family", "2015");
+        movieList.add(movie);
+
+        movie = new Model("Star Wars: Episode VII - The Force Awakens", "Action", "2015");
+        movieList.add(movie);
+
+        movie = new Model("Shaun the Sheep", "Animation", "2015");
+        movieList.add(movie);
+
+        movie = new Model("The Martian", "Science Fiction & Fantasy", "2015");
+        movieList.add(movie);
+
+        movie = new Model("Mission: Impossible Rogue Nation", "Action", "2015");
+        movieList.add(movie);
+
+        movie = new Model("Up", "Animation", "2009");
+        movieList.add(movie);
+
+        movie = new Model("Star Trek", "Science Fiction", "2009");
+        movieList.add(movie);
+
+        movie = new Model("The LEGO Movie", "Animation", "2014");
+        movieList.add(movie);
+
+        movie = new Model("Iron Man", "Action & Adventure", "2008");
+        movieList.add(movie);
+
+        movie = new Model("Aliens", "Science Fiction", "1986");
+        movieList.add(movie);
+
+        movie = new Model("Chicken Run", "Animation", "2000");
+        movieList.add(movie);
+
+        movie = new Model("Back to the Future", "Science Fiction", "1985");
+        movieList.add(movie);
+
+        movie = new Model("Raiders of the Lost Ark", "Action & Adventure", "1981");
+        movieList.add(movie);
+
+        movie = new Model("Goldfinger", "Action & Adventure", "1965");
+        movieList.add(movie);
+
+        movie = new Model("Guardians of the Galaxy", "Science Fiction & Fantasy", "2014");
+        movieList.add(movie);
+
+        mAdapter.notifyDataSetChanged();
+    }
 }
